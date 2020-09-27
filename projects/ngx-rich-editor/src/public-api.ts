457badad8ed9beaed5dc37ee4b3f1b9b7adf28d6
Artyom Gritsuk <postagric@gmail.com>
2020-09-27 13:36:05 +0300

    Remove unused service

--- projects/ngx-rich-editor/src/public-api.ts
+++ projects/ngx-rich-editor/src/public-api.ts
@@ -1,7 +1,2 @@
-/*
- * Public API Surface of ngx-rich-editor
- */
-
-export * from './lib/ngx-rich-editor.service';
 export * from './lib/ngx-rich-editor.component';
 export * from './lib/ngx-rich-editor.module';
