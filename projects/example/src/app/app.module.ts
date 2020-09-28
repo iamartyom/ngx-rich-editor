7f47f97b035e344c4b3f1817b8f942f1e15c8f53
Artyom Gritsuk <postagric@gmail.com>
2020-09-28 10:41:36 +0300

    Implement ControlValueAccessor in ngx-rich-editor component

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgxRichEditorModule } from 'ngx-rich-editor';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { NgxRichEditorModule } from 'ngx-rich-editor';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgxRichEditorModule,
   ],
   providers: [],
--- projects/ngx-rich-editor/src/lib/ngx-rich-editor/ngx-rich-editor.component.ts
+++ projects/ngx-rich-editor/src/lib/ngx-rich-editor/ngx-rich-editor.component.ts
@@ -1,10 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'ngx-rich-editor',
   templateUrl: './ngx-rich-editor.component.html',
-  styleUrls: ['./ngx-rich-editor.component.scss']
+  styleUrls: ['./ngx-rich-editor.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NgxRichEditorComponent),
+      multi: true
+    }
+  ]
 })
-export class NgxRichEditorComponent {
-  @Input() data: string;
+export class NgxRichEditorComponent implements ControlValueAccessor {
+  data: string;
+  disabled: boolean;
+
+  writeValue(obj: any): void {
+   this.data = obj;  
+  }
+
+  registerOnChange(fn: any) {
+
+  }
+
+  registerOnTouched(fn: any) {
+
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
